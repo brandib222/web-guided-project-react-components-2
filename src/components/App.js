64decf0cdba7a2a32fb2a9f11e965320616730d9
Greg Ball <gregball@Gregs-MBP.attlocal.net>
2021-11-02 13:14:28 -0400

    finished solution

--- src/components/App.js
+++ src/components/App.js
@@ -57,6 +57,7 @@ export default function App() {
 
       {/* 👉 7- Render the FriendsList component */}
       {/* What prop/props does FriendsList need? */}
+      <FriendsList friends = {friends} changeStatus = {changeStatus} />
     </div>
   )
 }
--- src/components/PetsList.js
+++ src/components/PetsList.js
@@ -1,13 +1,15 @@
 import React from 'react'
-// 👉 1- We need the Pet component
-
+// 👉 1- We need the Pet component (pets prop) IMPORT ITTTT
+import Pet from './Pet';
 export default function PetsList(props) {
   const { pets } = props
 
   return (
     <div className='list-pets-friends container'>
       {/* 2- Loop over the data generating a Pet element as you go */}
-      {/* What props does the Pet component expect? */}
+      {/* What props does the Pet component expect? 
+      below is the same as using curly brackets and return*/}
+      {pets.map(pet => <Pet pet = {pet} />)}
     </div>
   )
 }
